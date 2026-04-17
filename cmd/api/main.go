747d623d2c2483f2efc34968ef2251d69e3cf700
unknown <mutheedavid12@gmail.com>
2026-04-17 16:28:52 +0300

    feat: adds passes store to the main function

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/Davidmuthee12/socials/internal/env"
+	"github.com/Davidmuthee12/socials/internal/store"
 )
 
 func main() {
@@ -11,10 +12,14 @@ func main() {
 		addr: env.GetString("ADDR", ":8000"),
 	}
 
+	store := store.NewStorage(nil)
+
 	app := &application{
 		config: cfg,
+		store: store,
 	}
 
+	
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 }
