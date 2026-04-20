3d21c4ad72c747481484804403bb0ac58fb94b9a
unknown <mutheedavid12@gmail.com>
2026-04-20 16:09:44 +0300

    feat: adds new entry point for the new comments table schema

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -3,15 +3,24 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
 )
 
+var (
+	ErrNotFound = errors.New("record not found")
+)
 type Storage struct {
 	Posts interface {
+		GetByID(context.Context, int64) (*Post, error)
 		Create(context.Context, *Post) error
 	}
 
 	Users interface {
 		Create(context.Context, *User) error
+	}
+
+	Comments interface {
+		GetPostById( context.Context, int64) ([]Comment, error)
 	}
 }
 
@@ -20,5 +29,6 @@ func NewStorage(db *sql.DB) Storage{
 	return Storage {
 		Posts: &PostStore{db},
 		Users: &UserStore{db},
+		Comments: &CommentStore{db},
 	}
 }
