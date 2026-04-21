dd70868f08ba8be88915d34d544a4606a9bcaf3b
unknown <mutheedavid12@gmail.com>
2026-04-21 15:23:48 +0300

    feat: adds managing sql query timeouts

--- internal/store/posts.go
+++ internal/store/posts.go
@@ -30,6 +30,9 @@ func (s *PostStore) Create(ctx context.Context, post *Post) error {
 		VALUES ($1, $2, $3, $4) RETURNING id, created_at, updated_at
 	`  
 
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
 	err := s.db.QueryRowContext(
 		ctx,
 		query,
@@ -56,6 +59,9 @@ func (s *PostStore) GetByID(ctx context.Context, id int64) (*Post, error) {
 		FROM posts
 		WHere id = $1
 	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
 	var post Post
 
@@ -90,6 +96,9 @@ func (s *PostStore) DeletePost(ctx context.Context, postID int64) (error) {
 	query := `
 		DELETE FROM posts where id = $1
 	`
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
+
 	res, err := s.db.ExecContext(ctx, query, postID)
 	if err != nil {
 		return err
@@ -114,6 +123,9 @@ func (s *PostStore) UpdatePost(ctx context.Context, post *Post) error {
 		WHERE id = $3 AND version = $4
 		RETURNING version
 	`
+
+	ctx, cancel := context.WithTimeout(ctx, QueryTimeoutDuration)
+	defer cancel()
 
 	 err := s.db.QueryRowContext(
 		ctx,
