bb39ef644b4d59276a6afbea6dfb7134549dafe0
unknown <mutheedavid12@gmail.com>
2026-04-21 12:27:49 +0300

    feat: fixes spaces in users struct method

--- internal/store/users.go
+++ internal/store/users.go
@@ -9,8 +9,8 @@ type User struct {
 	ID int64 `json:"id"`
 	Username string `json:"username"`
 	Email string `json:"email"`
-	Password string `json: "-"`
-	CreatedAt string `json: "created_at"`
+	Password string `json:"-"`
+	CreatedAt string `json:"created_at"`
 }
 
 type UserStore struct {
