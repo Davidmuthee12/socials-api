630b4abace8f36cf4cc9d5c31a3541dd09f9235d
unknown <mutheedavid12@gmail.com>
2026-04-23 15:30:19 +0300

    feat: adds GetUserFeed to store handler

--- internal/store/storage.go
+++ internal/store/storage.go
@@ -18,6 +18,7 @@ type Storage struct {
 		Create(context.Context, *Post) error
 		DeletePost(context.Context, int64) error
 		UpdatePost(context.Context, *Post) error
+		GetUserFeed(context.Context, int64) ([]PostWithMetadata, error)
 	}
 
 	Users interface {
