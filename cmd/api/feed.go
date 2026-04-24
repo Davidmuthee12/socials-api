d4c3c52c2af6a924a7522366af759fa30898a2a4
unknown <mutheedavid12@gmail.com>
2026-04-24 17:08:19 +0300

    feat: documents feed handlers endpoints

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -6,6 +6,25 @@ import (
 	"github.com/Davidmuthee12/socials/internal/store"
 )
 
+// GetUserFeed godoc
+//
+//	@Summary		Get user feed
+//	@Description	Returns a paginated feed of posts for a user
+//	@Tags			users
+//	@Accept			json
+//	@Produce		json
+//	@Param			limit	query		int		false	"Items per page (1-20)"
+//	@Param			offset	query		int		false	"Offset"
+//	@Param			sort	query		string	false	"Sort order"	Enums(asc, desc)
+//	@Param			tags	query		string	false	"Comma-separated tags"
+//	@Param			search	query		string	false	"Search term"
+//	@Param			since	query		string	false	"Lower bound datetime (YYYY-MM-DD HH:MM:SS)"
+//	@Param			until	query		string	false	"Upper bound datetime (YYYY-MM-DD HH:MM:SS)"
+//	@Success		200		{array}		store.PostWithMetadata
+//	@Failure		400		{object}	error
+//	@Failure		500		{object}	error
+//	@Security		ApiKeyAuth
+//	@Router			/users/feed [get]
 func (app *application) getUserFeedHandler(w http.ResponseWriter, r *http.Request) {
 	fq := store.PaginatedFeedQuery{
 		Limit:  20,
