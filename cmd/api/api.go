b54f76ec0bf117d8cc7a2f00c9a72c7781f74646
unknown <mutheedavid12@gmail.com>
2026-04-25 17:28:54 +0300

    feat: adds new user authentication route

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -77,6 +77,10 @@ func (app *application) mount() http.Handler {
 				r.Get("/feed", app.getUserFeedHandler)
 			})
 
+			r.Route("/authentication", func(r chi.Router) {
+				r.Post("/users", app.registerUserHandler)
+			})
+
 		})
 	})
 
