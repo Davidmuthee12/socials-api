86f252bcf79f86130f3db233a59ef5884a7eb314
unknown <mutheedavid12@gmail.com>
2026-04-28 13:32:18 +0300

    feat(auth): add JWT authenticator implementation

---
+++ internal/auth/jwt.go
@@ -0,0 +1,29 @@
+package auth
+
+import "github.com/golang-jwt/jwt/v5"
+
+type JWTAuthenticator struct {
+	secret string
+	aud    string //audience
+	iss    string //issue
+}
+
+func NewJWTAuthenticator(secret, aud, iss string) *JWTAuthenticator {
+	return &JWTAuthenticator{secret, aud, iss}
+}
+
+func (a *JWTAuthenticator) GenerateToken(claims jwt.Claims) (string, error) {
+	// Use HMAC signing because AUTH_TOKEN_SECRET is configured as a shared secret.
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(a.secret))
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func (a *JWTAuthenticator) ValidateToken(token string) (*jwt.Token, error) {
+	return nil, nil
+}
