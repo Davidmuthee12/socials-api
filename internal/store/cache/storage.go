96fd587b6f1954e2c7bd374fbe4bd45ade853ec6
unknown <mutheedavid12@gmail.com>
2026-04-29 16:20:49 +0300

    feat: add redis-backed cache storage wrapper

---
+++ internal/store/cache/storage.go
@@ -0,0 +1,21 @@
+package redis
+
+import (
+	"context"
+
+	"github.com/Davidmuthee12/socials/internal/store"
+	"github.com/redis/go-redis/v9"
+)
+
+type Storage struct {
+	Users interface {
+		Get(context.Context, int64) (*store.User, error)
+		Set(context.Context, *store.User) error
+	}
+}
+
+func NewRedisStorage(rdb *redis.Client) Storage {
+	return Storage{
+		Users: &UserStore{rdb: rdb},
+	}
+}
