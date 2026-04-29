a985a0839e9a19ea59f9c78de4cf11d36dedafe9
unknown <mutheedavid12@gmail.com>
2026-04-29 16:20:42 +0300

    feat: add cache storage and redis config to application

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Davidmuthee12/socials/internal/auth"
 	"github.com/Davidmuthee12/socials/internal/mailer"
 	"github.com/Davidmuthee12/socials/internal/store"
+	cache "github.com/Davidmuthee12/socials/internal/store/cache"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
@@ -18,6 +19,7 @@ import (
 type application struct {
 	config        config
 	store         store.Storage
+	cacheStorage  cache.Storage
 	logger        *zap.SugaredLogger
 	mailer        mailer.Client
 	authenticator auth.Authenticator
@@ -31,6 +33,14 @@ type config struct {
 	mail        mailConfig
 	frontendURL string
 	auth        authConfig
+	redisCfg    redisConfig
+}
+
+type redisConfig struct {
+	addr    string
+	pw      string
+	db      int
+	enabled bool
 }
 
 type authConfig struct {
