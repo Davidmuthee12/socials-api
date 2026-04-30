e01bf8855ddd0d1335a1f090bc37a72ebd9941cd
unknown <mutheedavid12@gmail.com>
2026-04-30 14:45:08 +0300

    test: add API rate limiter middleware coverage

---
+++ cmd/api/api_test.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	ratelimiter "github.com/Davidmuthee12/socials/internal/rateLimiter"
+)
+
+func TestRateLimiterMiddleware(t *testing.T) {
+	cfg := config{
+		rateLimiter: ratelimiter.Config{
+			RequestPerTimeFrame: 20,
+			TimeFrame: time.Second * 5,
+			Enabled: true,
+		},
+		auth: authConfig{
+			basic: basicConfig{
+				user: "test-user",
+				pass: "test-pass",
+			},
+		},
+		addr: ":8080",
+	}
+
+	app := newTestApplication(t, cfg)
+	ts := httptest.NewServer(app.mount())
+	defer ts.Close()
+
+	client := &http.Client{}
+	mockIp := "192.168.1.1"
+	marginOfError := 2
+
+	for i := 0; i < cfg.rateLimiter.RequestPerTimeFrame+marginOfError; i++ {
+		req, err := http.NewRequest("GET", ts.URL+"/v1/health", nil)
+		if err != nil {
+			t.Fatalf("could not create request: %v", err)
+		}
+
+		req.Header.Set("X-Forwarded-For", mockIp)
+		req.SetBasicAuth(cfg.auth.basic.user, cfg.auth.basic.pass)
+
+		resp, err := client.Do(req)
+		if err != nil {
+			t.Fatalf("could not send request: %v", err)
+		}
+		defer resp.Body.Close()
+
+		if i < cfg.rateLimiter.RequestPerTimeFrame {
+			if resp.StatusCode != http.StatusOK {
+				t.Errorf("expected status OK; got %v", resp.Status)
+			}
+		} else {
+			if resp.StatusCode != http.StatusTooManyRequests {
+				t.Errorf("expected status Too Many Requests; got %v", resp.Status)
+			}
+		}
+	}
+	
+}
