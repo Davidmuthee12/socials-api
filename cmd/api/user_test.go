359ddf081285b976f18381ff496a32e187aec5ea
unknown <mutheedavid12@gmail.com>
2026-04-30 14:45:07 +0300

    test: update user handler tests to use configurable test application

--- cmd/api/user_test.go
+++ cmd/api/user_test.go
@@ -6,7 +6,8 @@ import (
 )
 
 func TestGetUser(t *testing.T) {
-	 app := newTestApplication(t)
+	cfg := config{}
+	 app := newTestApplication(t, cfg)
 	 mux := app.mount()
 
 	 testToken, err := app.authenticator.GenerateToken(nil) 
